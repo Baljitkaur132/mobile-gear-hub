c97a85c58666fb81a74dd27e5e9dfebfd835179f
Baljitkaur132 <baljitkaur@rrc.ca>
2026-03-28 00:42:02 -0500

    Add Product model with full CRUD in ActiveAdmin dashboard

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,18 +1,45 @@
 ActiveAdmin.register Product do
+  permit_params :name, :description, :price, :stock_quantity,
+                :category, :is_on_sale, :is_new, :image
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :name, :description, :price, :stock_quantity, :category, :is_on_sale, :is_new, :image
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:name, :description, :price, :stock_quantity, :category, :is_on_sale, :is_new, :image]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :category
+    column :price
+    column :stock_quantity
+    column :is_on_sale
+    column :is_new
+    actions
+  end
+
+  form do |f|
+    f.inputs "Product Details" do
+      f.input :name
+      f.input :description
+      f.input :price
+      f.input :stock_quantity
+      f.input :category
+      f.input :is_on_sale
+      f.input :is_new
+      f.input :image
+    end
+    f.actions
+  end
+
+  show do
+    attributes_table do
+      row :name
+      row :description
+      row :price
+      row :stock_quantity
+      row :category
+      row :is_on_sale
+      row :is_new
+      row :image
+      row :created_at
+      row :updated_at
+    end
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ApplicationRecord
+  def self.ransackable_attributes(auth_object = nil)
+    ["category", "created_at", "description", "id", "image", "is_new", "is_on_sale", "name", "price", "stock_quantity", "updated_at"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
 end
