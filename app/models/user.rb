fbbd199aff6d9f48e07441d310f1b9e86927c1a5
Baljitkaur132 <baljitkaur@rrc.ca>
2026-04-01 23:16:21 -0500

    Add ransackable_attributes to User model for admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,12 @@ class User < ApplicationRecord
   has_many :orders
 
   validates :name, presence: true
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["address", "city", "created_at", "email", "id", "name", "postal_code", "province_id", "updated_at"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["province", "orders"]
+  end
 end
