5b3de6193bda5a0f615deb9e00999bc4ba843231
Baljitkaur132 <baljitkaur@rrc.ca>
2026-04-01 22:06:59 -0500

    Fix Tag ransackable_attributes for admin tags page

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,12 @@ class Tag < ApplicationRecord
   has_many :product_tags
   has_many :products, through: :product_tags
   validates :name, presence: true, uniqueness: true
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "id", "name", "updated_at"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
 end
